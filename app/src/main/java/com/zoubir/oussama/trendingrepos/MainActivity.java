87382aef737f7933afc234086b3874dac81551ef
oussama zoubir <oussama zoubir>
2018-02-20 18:02:48 +0000

    adding INTERNET permission to manifest and creating MainActivity blank

---
+++ app/src/main/java/com/zoubir/oussama/trendingrepos/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zoubir.oussama.trendingrepos;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by OussamaZoubir on 2/20/2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
