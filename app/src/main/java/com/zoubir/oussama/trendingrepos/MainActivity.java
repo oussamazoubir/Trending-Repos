ed5423f8da5e34a40dc11586cb5c6d8db26013cf
oussama zoubir <oussama zoubir>
2018-02-20 18:29:40 +0000

    necessary variables for the list view in MainActivity

--- app/src/main/java/com/zoubir/oussama/trendingrepos/MainActivity.java
+++ app/src/main/java/com/zoubir/oussama/trendingrepos/MainActivity.java
@@ -1,16 +1,36 @@
 package com.zoubir.oussama.trendingrepos;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by OussamaZoubir on 2/20/2018.
  */
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private ProgressDialog pDialog;
+    private ListView lv;
+
+    // URL to get contacts JSON
+    private static String url = "https://api.github.com/search/repositories?q=created:>2017-12-22&sort=stars&order=desc?q=items";
+
+    ArrayList<HashMap<String, String>> itemList;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        itemList = new ArrayList<>();
+
+        lv = (ListView) findViewById(R.id.list);
     }
 }
